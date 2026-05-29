5a4f1f274d6b8a0fcf2ad340e76fdf0e199e3c7a
Paul Taylor <neon.taylor@gmail.com>
2026-05-29 14:36:35 -0400

    lib_numpy_utils.py: mapping dict from NumPy dtypes to NIFTI types
    Co-authored-by: Rick Reynolds <reynoldr@mail.nih.gov>
    Co-authored-by: Paul Taylor <neon.taylor@gmail.com>

--- lib_numpy_utils.py
+++ lib_numpy_utils.py
@@ -1,31 +1,210 @@
 #!/usr/bin/env python
 
+# Functions and objects for navigating NIFTI-format features, when
+# using Python's NumPy module. 
+# 
+# auth: PA Taylor (SSCC, NIMH, NIH, USA)
+#       RC Reynolds (SSCC, NIMH, NIH, USA)
+# ============================================================================
+
 import numpy   as     np
 
 from . import lib_simple_utils as lsu
 from . import lib_nifti_defs   as lnd
 
 # ============================================================================
+# mapping rules from NumPy Python types to NIFTI data types.
 
+# There are multiple dictionaries here, because there are (at least) a
+# few major ways that one might want to choose to map all possible
+# NumPy datatypes to NIFTI types. Some of these are due to constraints
+# within the available set of nifti types (and/or the plethora of
+# NumPy types). Some of these are due to practical software
+# considerations.  Some of the mappings might lead to lossy-ness,
+# which we denote here, as well.  Some of these are unavoidable, and
+# some are very unlikely.
+#
+# The structure of the dictionaries here is the same:
+#    key   : NumPy datatype (and this list could grow over time)
+#    value : list of 3 elements:
+#            [0] known NIFTI keyword within type and bitpix fields, to
+#                which the 'key' dtype will be mapped
+#            [1] name of NumPy dtype to which the 'key' dtype will be 
+#                mapped;  this might be different than the 'key' one by 
+#                necessity or choice/convenience
+#            [2] a string denoting whether the given dtype->type mapping
+#                will be:
+#                'noloss' : preserve all values,
+#                'lossy'  : will necessarily lose values, if the full
+#                           input domain is used
+#                'sysdep' : the input dtype has system-dependent properties,
+#                           and therefore may or may not be lossy, if the
+#                           full input domain is used                    
 
-
-DICT_np_dtype_to_nifti_type_strict = {
-    np.bool        : "DT_UNSIGNED_CHAR",
-    np.bool_       : "DT_UNSIGNED_CHAR",   # alias for np.bool
-    np.int8        : "DT_INT16",
-    np.int16       : "DT_INT16",
-    np.int32       : "DT_INT32",
-    np.int64       ; "DT_FLOAT32",
-    np.int_        ; "DT_FLOAT32",   # alias for np.int64
-    np.float16     : "DT_FLOAT32",
-    np.float32     : "DT_FLOAT32",
-    np.float64     : "DT_FLOAT64",
-    np.double      : "DT_FLOAT64",   # alias for np.float64
-    np.complex64   : "DT_COMPLEX64",
-    np.complex128  : "DT_COMPLEX128",
-    np.clongdouble : "DT_COMPLEX128",
+# how each known NumPy numerical datatype maps to NIFTI codes;
+#  the 'theoretical' list for many software
+DICT_np_dtype_to_nifti1_type = {
+    np.bool        : ["NIFTI_TYPE_UINT8", np.uint8, 
+                      'noloss'],
+    np.bool_       : ["NIFTI_TYPE_UINT8", np.uint8, 
+                      'noloss'],                              # alias for np.bool
+    np.uint8       : ["NIFTI_TYPE_UINT8", np.uint8, 
+                      'noloss'],
+    np.uint16      : ["NIFTI_TYPE_UINT16", np.uint16, 
+                      'noloss'],
+    np.uint32      : ["NIFTI_TYPE_UINT32", np.uint32, 
+                      'noloss'],
+    np.uintc       : ["NIFTI_TYPE_UINT32", np.uint32, 
+                      'noloss'],                              # alias for np.uint32
+    np.uint64      : ["NIFTI_TYPE_UINT64", np.uint64, 
+                      'noloss'],    
+    np.uint        : ["NIFTI_TYPE_UINT64",np.uint64, 
+                      'noloss'],                              # alias for np.int64
+    np.int8        : ["NIFTI_TYPE_INT8",  np.int8, 
+                      'noloss'],
+    np.int16       : ["NIFTI_TYPE_INT16", np.int16, 
+                      'noloss'],
+    np.int32       : ["NIFTI_TYPE_INT32", np.int32, 
+                      'noloss'],
+    np.intc        : ["NIFTI_TYPE_INT32", np.int32, 
+                      'noloss'],                              # alias for np.uint32
+    np.int64       : ["NIFTI_TYPE_INT64", np.int64, 
+                      'noloss'],    
+    np.int_        : ["NIFTI_TYPE_INT64", np.int64, 
+                      'noloss'],                              # alias for np.int64
+    np.long        : ["NIFTI_TYPE_INT64", np.int64, 
+                      'noloss'],                              # alias for np.int64  
+    np.longlong    : ["NIFTI_TYPE_INT64", np.int64, 
+                      'sysdep'],                              # tricky, sys-dep (loss?)
+    np.float16     : ["NIFTI_TYPE_FLOAT32", np.float32, 
+                      'noloss'],                              # NB: upgrade!
+    np.float32     : ["NIFTI_TYPE_FLOAT32", np.float32, 
+                      'noloss'],
+    np.float64     : ["NIFTI_TYPE_FLOAT64", np.float64, 
+                      'noloss'],
+    np.double      : ["NIFTI_TYPE_FLOAT64", np.float64, 
+                      'noloss'],                              # alias for np.float64
+    np.longdouble  : ["NIFTI_TYPE_FLOAT64", np.float64, 
+                      'sysdep'],                              # tricky, sys-dep
+    np.complex64   : ["NIFTI_TYPE_COMPLEX64", np.complex64, 
+                      'noloss'],
+    np.complex128  : ["NIFTI_TYPE_COMPLEX128", np.complex128, 
+                      'noloss'],
+    np.clongdouble : ["NIFTI_TYPE_COMPLEX128", np.complex128, 
+                      'sysdep'],                              # tricky, sys-dep
 }
 
+# how each known NumPy numerical datatype maps to NIFTI codes;
+# the 'in practice' list for many software
+DICT_np_dtype_to_nifti1_type_reduced = {
+    np.bool        : ["NIFTI_TYPE_UINT8", np.uint8, 
+                      'noloss'],
+    np.bool_       : ["NIFTI_TYPE_UINT8", np.uint8, 
+                      'noloss'],                             # alias for np.bool
+    np.uint8       : ["NIFTI_TYPE_UINT8", np.uint8, 
+                      'noloss'],
+    np.uint16      : ["NIFTI_TYPE_UINT16", np.uint16, 
+                      'noloss'],
+    np.uint32      : ["NIFTI_TYPE_UINT32", np.uint32, 
+                      'noloss'],
+    np.uintc       : ["NIFTI_TYPE_UINT32", np.uint32, 
+                      'noloss'],                             # alias for np.uint32
+    np.uint64      : ["NIFTI_TYPE_UINT32", np.uint32, 
+                      'lossy'],
+    np.uint        : ["NIFTI_TYPE_UINT32",np.uint32, 
+                      'noloss'],                             # alias for np.int64; loss
+    np.int8        : ["NIFTI_TYPE_INT8",  np.int8, 
+                      'noloss'],
+    np.int16       : ["NIFTI_TYPE_INT16", np.int16, 
+                      'noloss'],
+    np.int32       : ["NIFTI_TYPE_INT32", np.int32, 
+                      'noloss'],
+    np.intc        : ["NIFTI_TYPE_INT32", np.int32, 
+                      'noloss'],                             # alias for np.uint32
+    np.int64       : ["NIFTI_TYPE_INT32", np.int32, 
+                      'lossy'],
+    np.int_        : ["NIFTI_TYPE_INT32", np.int32, 
+                      'lossy'],                              # alias for np.int64
+    np.long        : ["NIFTI_TYPE_INT32", np.int32, 
+                      'lossy'],                              # alias for np.int64
+    np.longlong    : ["NIFTI_TYPE_INT32", np.int32, 
+                      'lossy'],                              # tricky, sys-dep
+    np.float16     : ["NIFTI_TYPE_FLOAT32", np.float32,
+                      'noloss'],                             # NB: upgrade!
+    np.float32     : ["NIFTI_TYPE_FLOAT32", np.float32, 
+                      'noloss'],
+    np.float64     : ["NIFTI_TYPE_FLOAT32", np.float32, 
+                      'lossy'],
+    np.double      : ["NIFTI_TYPE_FLOAT32", np.float32, 
+                      'lossy'],                              # alias for np.float64
+    np.longdouble  : ["NIFTI_TYPE_FLOAT32", np.float32, 
+                      'lossy'],                              # tricky, sys-dep
+    np.complex64   : ["NIFTI_TYPE_COMPLEX64", np.complex64, 
+                      'noloss'],
+    np.complex128  : ["NIFTI_TYPE_COMPLEX64", np.complex64, 
+                      'lossy'],
+    np.clongdouble : ["NIFTI_TYPE_COMPLEX64", np.complex64, 
+                      'lossy'],                              # tricky, sys-dep
+}
+
+# how each known NumPy numerical datatype maps to NIFTI codes;
+# the 'in practice' list for AFNI
+DICT_np_dtype_to_nifti1_type_reduced_afni = {
+    # MRI_byte
+    np.bool        : ["NIFTI_TYPE_UINT8", np.uint8, 
+                      'noloss'],
+    np.bool_       : ["NIFTI_TYPE_UINT8", np.uint8, 
+                      'noloss'],                             # alias for np.bool
+    np.uint8       : ["NIFTI_TYPE_UINT8", np.uint8, 
+                      'noloss'],
+    # MRI_short
+    np.int8        : ["NIFTI_TYPE_INT16", np.int16, 
+                      'noloss'],
+    np.int16       : ["NIFTI_TYPE_INT16", np.int16, 
+                      'noloss'],
+    # MRI_float
+    np.uint16      : ["NIFTI_TYPE_UINT16", np.float32, 
+                      'noloss'],
+    np.uint32      : ["NIFTI_TYPE_UINT32", np.float32, 
+                      'lossy'],
+    np.uintc       : ["NIFTI_TYPE_UINT32", np.float32, 
+                      'lossy'],                              # alias for np.uint32
+    np.uint64      : ["NIFTI_TYPE_UINT32", np.float32, 
+                      'lossy'],
+    np.uint        : ["NIFTI_TYPE_UINT32",np.float32, 
+                      'lossy'],                              # alias for np.int64
+    np.int32       : ["NIFTI_TYPE_INT32", np.float32, 
+                      'lossy'],
+    np.intc        : ["NIFTI_TYPE_INT32", np.float32, 
+                      'lossy'],                              # alias for np.uint32
+    np.int64       : ["NIFTI_TYPE_INT32", np.float32,
+                      'lossy'],
+    np.int_        : ["NIFTI_TYPE_INT32", np.float32,
+                      'lossy'],                              # alias for np.int64
+    np.long        : ["NIFTI_TYPE_INT32", np.float32,
+                      'lossy'],                              # alias for np.int64
+    np.longlong    : ["NIFTI_TYPE_INT32", np.float32,
+                      'lossy'],                              # tricky, sys-dep
+    np.float16     : ["NIFTI_TYPE_FLOAT32", np.float32,
+                      'noloss'],                             # NB: upgrade!
+    np.float32     : ["NIFTI_TYPE_FLOAT32", np.float32,
+                      'noloss'],
+    np.float64     : ["NIFTI_TYPE_FLOAT32", np.float32,
+                      'lossy'],
+    np.double      : ["NIFTI_TYPE_FLOAT32", np.float32,
+                      'lossy'],                              # alias for np.float64
+    np.longdouble  : ["NIFTI_TYPE_FLOAT32", np.float32,
+                      'lossy'],                              # tricky, sys-dep
+    # MRI_complex
+    np.complex64   : ["NIFTI_TYPE_COMPLEX64", np.complex64,
+                      'noloss'],
+    np.complex128  : ["NIFTI_TYPE_COMPLEX64", np.complex64,
+                      'lossy'],
+    np.clongdouble : ["NIFTI_TYPE_COMPLEX64", np.complex64,
+                      'lossy'],                              # tricky, sys-dep
+    # MRI_rgb  --- TBD
+    # MRI_rgba --- not possible
+}
 
 
 # ============================================================================
@@ -105,10 +284,11 @@ ytype : str
     return y, ytype
 
 
-def def translate_numpy_dtype_to_nifti(d, verb=1):
-    """The input d is likely the value of the dtype attribute of some
-numpy array (e.g., NPARRAY.dtype).  This function determines which
-NIFTI type and bitpix values it will correspond to.
+def def translate_numpy_dtype_to_nifti(din, verb=1):
+    """The input din is a numpy dtype, likely obtained as the
+attribute describing the elements of some numpy array (e.g.,
+NPARRAY.dtype).  This function determines which NIFTI type and bitpix
+values it will correspond to.
 
 This function also outputs a new type that the array should be
 converted to, for the purpose of writing, if necessary.  (Otherwise,
@@ -116,7 +296,7 @@ the value returned for that information is: None)
 
 Parameters
 ----------
-d : (np.ndarray).dtype
+din : (np.ndarray).dtype
     a NumPy array's dtype
 verb : int
     amount of verbosity to use in general processing
@@ -129,8 +309,10 @@ nifti_type : int
     code for the NIFTI header field: type
 nifti_bitpix : int
     code for the NIFTI header field: bitpix
-newtype : dtype
-    a NumPy array dtype that the data array of d should be converted to
+dout : (np.ndarray).dtype
+    a NumPy array's dtype
+newtype : int
+    0 for din==dout, else nonzero
 
     """
 
@@ -139,53 +321,24 @@ newtype : dtype
     nifti_bitpix = ''
     newtype      = None
 
-    BAD_RETURN  = (-1, nifti_type, nifti_bitpix, newtype)
+    BAD_RETURN = (-1, nifti_type, nifti_bitpix, newtype)
 
 
-    if np.issubtype(d, bool) :
-        # will always go for float32, even though current Python
-        # floats default as float32
-        nifti_type   = lnd.DICT_nifti_type["****"]
-        nifti_bitpix = lnd.DICT_nifti_bitpix["****"]
-        newtype = np.*****
-
-    elif np.issubtype(d, int) :
-        if np.issubdtype(d, np.int8) or np.issubdtype(d, np.int16) :
-            nifti_type   = lnd.DICT_nifti_type["DT_SIGNED_SHORT"]
-            nifti_bitpix = lnd.DICT_nifti_bitpix["DT_SIGNED_SHORT"]
-            newtype = None
-        elif np.issubdtype(d, np.int32) :
-            nifti_type   = lnd.DICT_nifti_type["DT_SIGNED_INT"]
-            nifti_bitpix = lnd.DICT_nifti_bitpix["DT_SIGNED_INT"]
-            newtype = None
-        elif np.issubdtype(d, np.int64) :
-            # note this major change in type; might be lossy
-            nifti_type   = lnd.DICT_nifti_type["DT_FLOAT32"]
-            nifti_bitpix = lnd.DICT_nifti_bitpix["DT_FLOAT32"]
-            newtype = np.float32
-            lsu.WP("dtype int64 -> float32, which might be lossy")
-
-    elif np.issubtype(d, float) :
-        # will always go for float32, even though current Python
-        # floats default as float32
-        nifti_type   = lnd.DICT_nifti_type["DT_FLOAT"]
-        nifti_bitpix = lnd.DICT_nifti_bitpix["DT_FLOAT"]
-        newtype = None
-        if np.issubtype(d, np.float64) :
-            lsu.WP("dtype float64 -> float32, which might be lossy")
-            newtype = np.float32
-        elif np.issubtype(d, np.float16) :
-            newtype = np.float32
-    
-    elif np.issubtype(d, complex) :
-        nifti_type   = lnd.DICT_nifti_type["DT_COMPLEX"]
-        nifti_bitpix = lnd.DICT_nifti_bitpix["DT_COMPLEX"]
-        newtype = np.float32
-
+    if din in DICT_np_dtype_to_nifti_type_strict.keys():
+        type_key = DICT_np_dtype_to_nifti_type_strict[din]
     else:
-        msg = "Surprised to see this dtype: {}. Exiting".format(d)
-        ab.EP1(msg)
+        # **** TEMPORARY: probably have more conditions here
+        lsu.EP1("Unknown dtype: {}".format(din))
         return BAD_RETURN
+                
+    if din != dout :
+        if verb :
+            msg = "converting dtype {} -> {}".format(d, type_key)
+            lsu.WP(msg)
+
+    nifti_type   = lnd.DICT_nifti_type[dnifti]
+    nifti_bitpix = lnd.DICT_nifti_bitpix[dnifti]
+
 
 
     return 0, nifti_type, nifti_bitpix, newtype
